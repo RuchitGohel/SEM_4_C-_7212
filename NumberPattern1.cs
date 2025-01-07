7bc17ab47555700f6f59d67f4c85c02def7bf61a
RuchitGohel <141817181+RuchitGohel@users.noreply.github.com>
2025-01-07 08:32:08 +0530

    Add files via upload

---
+++ NumberPattern1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPattern1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 1;
+            for (int i = 0; i<6;i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write(k++ + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NumberPattern2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPattern2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 1;
+            int i = 1, j = 1;
+            for(i = 1; i < 6; i++)
+            {
+                
+                for (j = 1; j <= i; j++)
+                { 
+                 Console.Write(k-- + " ");
+                }
+                k += i + j;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
