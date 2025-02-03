df1fd870c952e1763f868702cda9fb3f04964985
RuchitGohel <141817181+RuchitGohel@users.noreply.github.com>
2025-02-03 08:47:32 +0530

    Add files via upload

---
+++ Unit_3/Program_15.cs
@@ -0,0 +1,40 @@
+﻿// Program - 14 : Write a program to check weather a number is palindrome
+// or not
+// e.g. input: 121, output: It is
+// palindrome input:124, output: It is
+// not palindrome
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_13
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            int sum = 0, rev = 0, temp = 0;
+            Console.WriteLine("Enter a number to check whether it is palindrome or not.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //rev = num;
+            while(num != 0)
+            {
+                temp = num % 10;
+                sum = temp + (sum * 10);
+                num = num / 10;  
+            }
+            if(rev == num)
+            {
+                Console.WriteLine("The number" + num + "is palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("It is not a palindrome number.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Unit_3/Program_16.cs
@@ -0,0 +1,41 @@
+﻿// Program - 16 : Write a program to check weather a number is ArmStrong or not
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_13
+{
+    internal class Program_16
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check whether it is armstrong or not.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int temp = 0, m = 0, arm = 0;
+            arm = num;
+
+            while(num != 0)
+            {
+                m = num % 10;
+                temp = temp + (m * m * m);
+                num = num / 10;
+            }
+            if (arm == temp)
+            {
+                Console.WriteLine("It is an Armstrong number.");
+            }
+            else
+            {
+                Console.WriteLine("It is not an Armstrong number.");
+            }
+            Console.ReadLine();
+        }
+    
+    }
+    }
+
