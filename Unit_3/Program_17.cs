2cda0384964de63b19827cb19e2e5097610b4b04
RuchitGohel <141817181+RuchitGohel@users.noreply.github.com>
2025-03-18 07:47:01 +0530

    Add files via upload

---
+++ Unit_3/Program_17.cs
@@ -0,0 +1,38 @@
+﻿// Program - 17 : Write a program to display maximum number from 5
+// numbers inputted by user by using the concept of arrays
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Program_13
+{
+    internal class Program_17
+    {
+        static void Main4(string[] args)
+        {
+            int[] num = new int[5];
+            Console.WriteLine("Enter 5 elements for the array:");
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            //Console.WriteLine("The maximum number among the 5 elements is:");
+
+            int max = num[0];
+
+            for(int i = 0; i < num.Length; i++)
+            {
+                if (num[i] > max)
+                {
+                    max = num[i];
+                } 
+            }
+            Console.WriteLine($"Maximum value among these is :{max}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Unit_3/Program_18.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_13
+{
+    internal class Program_18
+    {
+        static void Main2(string[] args)
+        {
+            int[] num = new int[5];
+            Console.WriteLine("Enter 5 elements for the array:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            //Console.WriteLine("The maximum number among the 5 elements is:");
+
+            int min = num[0];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] < min)
+                {
+                    min = num[i];
+                }
+            }
+            Console.WriteLine($"Minimum value among these is :{min}");
+            Console.ReadLine();
+        }
+    }
+}
